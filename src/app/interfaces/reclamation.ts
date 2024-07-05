f80e8162248cf6e7cdc473baae269d4aa8be76a4
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2024-07-05 22:53:20 +0200

    Update reclamation.ts

--- src/app/interfaces/reclamation.ts
+++ src/app/interfaces/reclamation.ts
@@ -1,13 +1,21 @@
 // src/interfaces/reclamation.ts
 
+export  interface Reclamation  {
+  id?: string;
+  expediteur: any;
+  titre: any;
+  description?: any;
+  type: any;
+  etat?: 'En attente' | 'Traiter';
 
-  export interface Reclamation {
-    id?: string; // ID optionnel
-    titre: string;
-    description: string;
-    createdAt: Date;
-    updatedAt?: Date; // updatedAt optionnel
-    status: 'OPEN' | 'CLOSED';
-    response?: string; // response optionnel
-    user_Id: string; // ID de l'utilisateur
+
+  // export interface Reclamation {
+  //   id?: string; // ID optionnel
+  //   titre: string;
+  //   description: string;
+  //   createdAt: Date;
+  //   updatedAt?: Date; // updatedAt optionnel
+  //   status: 'OPEN' | 'CLOSED';
+  //   response?: string; // response optionnel
+  //   user_Id: string; // ID de l'utilisateur
 }
